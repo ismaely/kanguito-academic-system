2d8439166a1c272e9db47af439691ec017fb44da
ismaely <7ilipe@gmail.com>
2021-12-28 08:34:29 +0100

    add files new

--- arquivo/urls.py
+++ arquivo/urls.py
@@ -7,5 +7,6 @@ app_name = 'arquivo'
 
 urlpatterns = [
   path('registar_arquivos/', views.registar_arquivos, name='registar-arquivos'),
+  path('atualizar_arquivos/<int:pk>/', views.atualizar_arquivos, name='atualizar-arquivos'),
 
 ]
