1be12a79d3d9b4629dcc3a1023d0790c4d08dc8f
ismaely <7ilipe@gmail.com>
2021-12-28 17:10:37 +0100

    list files

--- arquivo/urls.py
+++ arquivo/urls.py
@@ -8,5 +8,8 @@ app_name = 'arquivo'
 urlpatterns = [
   path('registar_arquivos/', views.registar_arquivos, name='registar-arquivos'),
   path('atualizar_arquivos/<int:pk>/', views.atualizar_arquivos, name='atualizar-arquivos'),
-
+  path('listar_arquivos/', views.listar_arquivos, name='listar-arquivos'),
+  path('atualizar_arquivos/<int:pk>/', views.atualizar_arquivos, name='atualizar-arquivos'),
+  path('eliminar_arquivos/<int:pk>/', views.eliminar_arquivos, name='eliminar-arquivos'),
+  path('visualizar_arquivo/<int:pk>/', views.visualizar_arquivo, name='visualizar-arquivo'),
 ]
