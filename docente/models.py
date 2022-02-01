d8f6ea8b72caee106bb6371da0812ef871b93d86
ismaely <7ilipe@gmail.com>
2022-02-01 13:12:10 +0100

    add model orientador

--- docente/models.py
+++ docente/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from pessoa.models import Pessoa
 from config.models import Periodo
 from unidade_curricular.models import UnidadeCurricular
+from curso.models import Curso
 
 # Create your models here.
 
@@ -26,6 +27,13 @@ class Estado_Docente(models.Model):
     def __str__ (self):
         return "%s" % (self.nome)
 
+
+class Estado_Orientador(models.Model):
+    nome = models.CharField(max_length=50)
+    opcao = models.CharField(max_length=160, blank=True, null=True)
+    def __str__ (self):
+        return "%s" % (self.nome)
+
 class Docente(models.Model):
     pessoa= models.ForeignKey(Pessoa, on_delete=models.CASCADE, parent_link=True)
     numero_docente= models.CharField(max_length=10, null=True)
@@ -38,6 +46,17 @@ class Docente(models.Model):
 
     def __str__ (self):
         return '%d'  % (self.id)
+
+class Orientador(models.Model):
+    docente = models.ForeignKey(Docente, on_delete=models.CASCADE, parent_link=True)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE, parent_link=True)
+    data_limite = models.DateField(default=timezone.now)
+    estado = models.ForeignKey(Estado_Orientador, on_delete=models.CASCADE, parent_link=True)
+    created = models.DateField(blank=True, null=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    def __str__ (self):
+        return self.id
 
 
 """class Unidade_Curricular_Docente(models.Model):
